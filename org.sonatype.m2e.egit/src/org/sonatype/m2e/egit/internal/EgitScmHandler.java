50c44c1767d762f571415c0d1a8248888b47bf6f
Frank Jakop <frank.jakop@tolina.de>
2012-12-10 15:29:43 +0100

    whenever there's a <tag>...</tag> entry in the dependency's pom we do not want to use "master" but the ref corresponding to that tag, so we checkout this ref

--- org.sonatype.m2e.egit/src/org/sonatype/m2e/egit/internal/EgitScmHandler.java
+++ org.sonatype.m2e.egit/src/org/sonatype/m2e/egit/internal/EgitScmHandler.java
@@ -13,16 +13,19 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.egit.core.op.BranchOperation;
 import org.eclipse.egit.core.op.CloneOperation;
 import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileBasedConfig;
 import org.eclipse.jgit.storage.file.FileRepository;
 import org.eclipse.jgit.transport.URIish;
@@ -31,120 +34,143 @@ import org.eclipse.m2e.scm.spi.ScmHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * A SCM handler to enable M2Eclipse to checkout using EGit.
  */
 @SuppressWarnings("restriction")
 public class EgitScmHandler extends ScmHandler {
 
-  private final Logger log = LoggerFactory.getLogger(EgitScmHandler.class);
+	private final Logger log = LoggerFactory.getLogger(EgitScmHandler.class);
 
-  public static final String GIT_SCM_ID = "scm:git:";
+	public static final String GIT_SCM_ID = "scm:git:";
 
-  @Override
-  public void checkoutProject(MavenProjectScmInfo info, File location, IProgressMonitor monitor) throws CoreException,
-      InterruptedException {
-    log.debug("Checking out project from {} to {}", info, location);
+	@Override
+	public void checkoutProject(final MavenProjectScmInfo info, File location, IProgressMonitor monitor) throws CoreException,
+			InterruptedException {
+		log.debug("Checking out project from {} to {}", info, location);
 
-    SubMonitor pm = SubMonitor.convert(monitor, 100);
-    try {
-      URIish uri = getUri(info);
+		final SubMonitor pm = SubMonitor.convert(monitor, 100);
+		try {
+			URIish uri = getUri(info);
 
-      String refName = getRefName(info);
+			String refName = getRefName(info);
 
       CloneOperation clone = new CloneOperation(uri, true /* allSelected */, new ArrayList<Ref>(), location, refName,
           "origin", getTimeout());
       clone.run(pm.newChild(99));
+			
+			clone.addPostCloneTask(new CloneOperation.PostCloneTask() {
+				
+				public void execute(Repository repository, IProgressMonitor monitor) throws CoreException {
+					
+					// whenever there's a <tag>...</tag> entry in the dependency's pom
+					// we do not want to use "master" but the ref corresponding to that
+					// tag, so we checkout this ref
+					String revision = info.getRevision();
+					if (revision != null && revision.trim().length() > 0) {
 
-      fixAutoCRLF(clone.getGitDir());
-    } catch(InvocationTargetException e) {
-      Throwable cause = e.getTargetException();
+						Map<String, Ref> tags = repository.getTags();
+						Ref ref = tags.get(info.getRevision());
+
+						BranchOperation branch = new BranchOperation(repository, ref.getName());
+						branch.execute(pm.newChild(99));
+					}
+				}
+			});
+			
+			clone.run(pm.newChild(50));
+
+			
+
+			fixAutoCRLF(clone.getGitDir());
+		} catch (InvocationTargetException e) {
+			Throwable cause = e.getTargetException();
       throw new CoreException(new Status(IStatus.ERROR, getClass().getName(), cause.getMessage(), cause));
-    } catch(IOException e) {
+		} catch (IOException e) {
       throw new CoreException(new Status(IStatus.ERROR, getClass().getName(), e.getMessage(), e));
-    } catch(URISyntaxException e) {
+		} catch (URISyntaxException e) {
       throw new CoreException(new Status(IStatus.ERROR, getClass().getName(), e.getMessage(), e));
-    } catch(InterruptedException e) {
-      // The monitor was canceled
-    } finally {
-      pm.done();
-    }
-  }
+		} catch (InterruptedException e) {
+			// The monitor was canceled
+		} finally {
+			pm.done();
+		}
+	}
 
-  protected int getTimeout() {
-    return 30;
-  }
+	protected int getTimeout() {
+		return 30;
+	}
 
-  protected URIish getUri(MavenProjectScmInfo info) throws URISyntaxException {
-    String url = info.getRepositoryUrl();
-    url = normalizeUri(url);
+	protected URIish getUri(MavenProjectScmInfo info) throws URISyntaxException {
+		String url = info.getRepositoryUrl();
+		url = normalizeUri(url);
 
-    URIish uri = new URIish(url);
+		URIish uri = new URIish(url);
 
     if(isProtocolAuthAware(uri.getScheme()) && info.getUsername() != null && info.getUsername().length() > 0) {
       if(uri.getUser() == null || info.getUsername().equals(uri.getUser())) {
-        uri = uri.setUser(info.getUsername());
-      }
-    }
+				uri = uri.setUser(info.getUsername());
+			}
+		}
 
-    return uri;
-  }
+		return uri;
+	}
 
-  protected String normalizeUri(String uri) throws URISyntaxException {
-    if(!uri.startsWith(GIT_SCM_ID)) {
-      return uri;
-    }
+	protected String normalizeUri(String uri) throws URISyntaxException {
+		if (!uri.startsWith(GIT_SCM_ID)) {
+			return uri;
+		}
 
-    uri = uri.substring(GIT_SCM_ID.length());
-    if(uri.startsWith("file:") && !uri.startsWith("file:///")) {
-      throw new URISyntaxException(uri, "Invalid git URI");
-    }
+		uri = uri.substring(GIT_SCM_ID.length());
+		if (uri.startsWith("file:") && !uri.startsWith("file:///")) {
+			throw new URISyntaxException(uri, "Invalid git URI");
+		}
 
-    URIish gitUri = new URIish(uri);
-    if(gitUri.getScheme() == null) {
-      if(gitUri.getHost() == null || "file".equals(gitUri.getHost())) {
-        gitUri = gitUri.setHost(null);
-        gitUri = gitUri.setScheme("file");
-      } else {
-        // This must be an scp-like syntax git URL
+		URIish gitUri = new URIish(uri);
+		if (gitUri.getScheme() == null) {
+			if (gitUri.getHost() == null || "file".equals(gitUri.getHost())) {
+				gitUri = gitUri.setHost(null);
+				gitUri = gitUri.setScheme("file");
+			} else {
+				// This must be an scp-like syntax git URL
         // See http://www.kernel.org/pub/software/scm/git/docs/git-clone.html#_git_urls_a_id_urls_a
-        gitUri = gitUri.setScheme("ssh");
-      }
-    }
-    return gitUri.toString();
-  }
+				gitUri = gitUri.setScheme("ssh");
+			}
+		}
+		return gitUri.toString();
+	}
 
-  protected boolean isProtocolAuthAware(String protocol) {
-    return !"file".equalsIgnoreCase(protocol);
-  }
+	protected boolean isProtocolAuthAware(String protocol) {
+		return !"file".equalsIgnoreCase(protocol);
+	}
 
-  protected String getRefName(MavenProjectScmInfo info) {
-    String branch = info.getBranch();
+	protected String getRefName(MavenProjectScmInfo info) {
 
-    if(branch == null || branch.trim().length() == 0) {
-      branch = Constants.MASTER;
-    }
+		String branch = info.getBranch();
 
-    if(!branch.startsWith(Constants.R_REFS)) {
-      branch = Constants.R_HEADS + branch;
-    }
+		if (branch == null || branch.trim().length() == 0) {
+			branch = Constants.MASTER;
+		}
 
-    return branch;
-  }
+		if (!branch.startsWith(Constants.R_REFS)) {
+			branch = Constants.R_HEADS + branch;
+		}
 
-  protected void fixAutoCRLF(File gitDirectory) throws IOException {
+		return branch;
+	}
+
+	protected void fixAutoCRLF(File gitDirectory) throws IOException {
     // jgit does not have support for core.autocrlf but it sets the core.autocrlf=false in the local git
     // repository config (https://bugs.eclipse.org/bugs/show_bug.cgi?id=301775).
-    // We need to unset it.
-    FileRepository localRepository = new FileRepository(gitDirectory);
-    try {
-      FileBasedConfig localConfig = localRepository.getConfig();
+		// We need to unset it.
+		FileRepository localRepository = new FileRepository(gitDirectory);
+		try {
+			FileBasedConfig localConfig = localRepository.getConfig();
       localConfig.unset(ConfigConstants.CONFIG_CORE_SECTION, null, ConfigConstants.CONFIG_KEY_AUTOCRLF);
-      localConfig.save();
-    } finally {
-      localRepository.close();
-    }
-  }
+			localConfig.save();
+		} finally {
+			localRepository.close();
+		}
+	}
 
 }
